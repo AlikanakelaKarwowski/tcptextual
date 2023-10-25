04a69fe7ad46f5463f3f7ba07939414f29704176
A.Karwowski <akk42@zips.uakron.edu>
2023-10-24 22:25:04 -0400

    successfully create a config and save it as a local json file

--- config_generator.py
+++ config_generator.py
@@ -1,7 +1,7 @@
 import os
 import json
 from pydantic import BaseModel
-
+from pydantic.json import pydantic_encoder
 class Controller_Config(BaseModel):
     command_name: str
     keys: list[str]
@@ -9,17 +9,21 @@ class Controller_Config(BaseModel):
     cooldown: float | None
     chance: float | None
 
-def create_config(configs: list[Controller_Config], config_name:str,  path:str) -> list[Controller_Config]:
+
+def create_config(configs: list[Controller_Config], config_name:str,  path:str | None) -> list[Controller_Config]:
     script_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_dir, path, f"{config_name}.json")
-    with open(file_path, 'w') as f:
-        serialized = ([config.model_dump_json for config in configs])
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'w+') as f:
+        serialized = json.dumps(configs, default=pydantic_encoder)
         f.write(serialized)
 
 if __name__ == "__main__":
     configs = [
-        Controller_Config(command_name="test", keys=["k"], duration=1, cooldown=30, chance=0.5),
-        Controller_Config(command_name="test", keys=["e"], duration=1, cooldown=30, chance=0.5),
-        Controller_Config(command_name="test", keys=["y"], duration=1, cooldown=1, chance=0.5),
+        Controller_Config(command_name="kill", keys=["k"], duration=1, cooldown=30, chance=0.5),
+        Controller_Config(command_name="everyone", keys=["e"], duration=1, cooldown=30, chance=0.5),
+        Controller_Config(command_name="you", keys=["y"], duration=1, cooldown=1, chance=0.5),
+        Controller_Config(command_name="see", keys=["s"], duration=1, cooldown=1, chance=0.5),
+
     ]
-    
+    create_config(configs, "test", "config")
